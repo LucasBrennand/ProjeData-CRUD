32ba2d3da91c3d78f9d106cb0c5469d6cfee4456
lucasbrennand <lucasbrennand.barbosa@gmail.com>
2026-03-03 09:24:54 -0300

    Setting up app properties

---
+++ src/main/java/com/lucas/projedatacrud/ProjedataCrudApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.projedatacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjedataCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjedataCrudApplication.class, args);
+    }
+
+}
