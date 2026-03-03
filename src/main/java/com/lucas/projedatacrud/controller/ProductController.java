570d385df369a72a93cb4a9bb4171ce916db690c
lucasbrennand <lucasbrennand.barbosa@gmail.com>
2026-03-03 10:27:06 -0300

    Create productController

---
+++ src/main/java/com/lucas/projedatacrud/controller/ProductController.java
@@ -0,0 +1,27 @@
+package com.lucas.projedatacrud.controller;
+
+import com.lucas.projedatacrud.model.Product;
+import com.lucas.projedatacrud.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+@CrossOrigin(origins = "*")
+public class ProductController {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @GetMapping
+    public List<Product> getAll() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public Product create(@RequestBody Product product) {
+        return repository.save(product);
+    }
+}
