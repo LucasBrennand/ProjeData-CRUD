f31f03dcc8920ea89553101d35d916f4c4c4c745
lucasbrennand <lucasbrennand.barbosa@gmail.com>
2026-03-03 09:28:19 -0300

    Created RawMaterial in model

--- src/main/java/com/lucas/projedatacrud/model/RawMaterial.java
+++ src/main/java/com/lucas/projedatacrud/model/RawMaterial.java
@@ -1,4 +1,19 @@
 package com.lucas.projedatacrud.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "raw_materials")
+@Data
 public class RawMaterial {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int quantity;
 }
